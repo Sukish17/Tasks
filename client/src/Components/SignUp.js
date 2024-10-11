54ec651d83e1d9e17fa1d5e7ea5dcf336b1969dc
Sukish17 <165570197+Sukish17@users.noreply.github.com>
2024-10-11 11:35:44 +0530

    Update SignUp.js

--- client/src/Components/SignUp.js
+++ client/src/Components/SignUp.js
@@ -13,7 +13,7 @@ function SignUp(){
 
     const handleSignup = (e) => {
         e.preventDefault();
-        axios.post("http://localhost:3001/signup", { name, email, password })
+        axios.post("https://server-taupe-chi.vercel.app/signup", { name, email, password })
             .then(result => {
                 if (result.status === 201) {
                     navigate("/login");
